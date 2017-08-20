cd9e89a770f8bc099ebab5e87b099a10ddc8722c
Unknown <joeyskuo@gmail.com>
2017-08-19 22:50:04 -0700

    added note component; destructured declarations

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Form, FormControl, Button} from 'react-bootstrap';
+import Note from './Note';
 
+const roar = (message) => {
+    console.log(message);
+}
+roar('roaaaar!');
 
 class App extends Component {
 
-    render(){
-        return(
-         <div>
-             <h2>Note to Self</h2>
-            <Form>
-                <FormControl />
-                <Button>Submit</Button>
-            </Form>
-         </div>
+    constructor() {
+        super();
+
+        this.state = {
+            text: '',
+            notes: []
+        }
+    }
+
+    submit() {
+        const {notes, text} = this.state;
+
+        notes.push({text});
+
+        this.setState({notes})
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Note to Self</h2>
+                <Form inline>
+                    <FormControl onChange={event => {
+                        this.setState({text: event.target.value})
+                    }}/>
+                    {' '}
+                    <Button onClick={() => this.submit()}>Submit</Button>
+                </Form>
+                {
+                    this.state.notes.map((note, index) => {
+                        return (
+                            <Note key={index} note={note}/>
+                        )
+                    })
+                }
+            </div>
         )
     }
 }
